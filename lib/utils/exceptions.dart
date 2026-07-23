ce50c086a8558289d9ebfd7c7e7d18f284f2bec6
Muhammad Talha <240217552+shekhtalha789-glitch@users.noreply.github.com>
2026-07-24 03:03:20 +0500

    .

---
+++ lib/utils/exceptions.dart
@@ -0,0 +1,15 @@
+class NetworkException implements Exception {
+  final String message;
+  NetworkException(this.message);
+
+  @override
+  String toString() => message;
+}
+
+class ServerException implements Exception {
+  final String message;
+  ServerException(this.message);
+
+  @override
+  String toString() => message;
+}
